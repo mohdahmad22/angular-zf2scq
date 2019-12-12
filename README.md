0c0b8e9b9f79a5744fb86c5e9fff74e1d9e41531
mohdahmad22 <khanahmad545@gmail.com>
2019-12-12 14:16:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zf2scq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zf2scq)
---
+++ src/app/payls/payls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payls',
+  templateUrl: './payls.component.html',
+  styleUrls: ['./payls.component.css']
+})
+export class PaylsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
